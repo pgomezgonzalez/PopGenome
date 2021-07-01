1611ee99802e4b4d1140a6e795ee3ebfe95ec1ea
pgomezgonzalez <66364698+pgomezgonzalez@users.noreply.github.com>
2021-07-01 14:25:59 +0100

    Update popgen_ins.R

--- R/popgen_ins.R
+++ R/popgen_ins.R
@@ -1,4 +1,4 @@
-popgen <- function(Code_matrix,Populations=FALSE,outgroup=FALSE,methods=FALSE,include.unknown=TRUE,gff=FALSE,FAST,SNP.DATA){
+popgen_ins <- function(Code_matrix,Populations=FALSE,outgroup=FALSE,methods=FALSE,include.unknown=TRUE,gff=FALSE,FAST,SNP.DATA){
 
 
 ## KONVERTING ##########################################
