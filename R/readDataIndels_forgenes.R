b63f7077c3e136fac3d5dac7969d932f218ddc4f
pgomezgonzalez <66364698+pgomezgonzalez@users.noreply.github.com>
2021-07-06 11:24:01 +0100

    Update readDataIndels_forgenes.R

--- R/readDataIndels_forgenes.R
+++ R/readDataIndels_forgenes.R
@@ -1,4 +1,4 @@
-readDataIndels_forgenes <- function(vcf,path,populations=FALSE,outgroup=FALSE,include.unknown=FALSE,gffpath=FALSE,format="fasta",parallized=FALSE,progress_bar_switch=TRUE,
+readDataIndels_forgenes <- function(path,populations=FALSE,outgroup=FALSE,include.unknown=FALSE,gffpath=FALSE,format="fasta",parallized=FALSE,progress_bar_switch=TRUE,
 FAST=FALSE,big.data=FALSE,SNP.DATA=FALSE){
 #INDELS MODIFIED#
 
